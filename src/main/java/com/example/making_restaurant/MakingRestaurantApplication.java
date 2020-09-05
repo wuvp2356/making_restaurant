857ab66155588add8fd0b76c0b8206440e664e90
wuvp2356 <roleplaying1995@gmail.com>
2020-09-06 01:33:19 +0900

    making_restaurant1

---
+++ src/main/java/com/example/making_restaurant/MakingRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.example.making_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakingRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakingRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/making_restaurant/MakingRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.making_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakingRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
