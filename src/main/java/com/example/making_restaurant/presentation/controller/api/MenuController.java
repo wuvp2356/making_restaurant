8775315f156796f11e3040e79877dc951c6f39fb
wuvp2356 <roleplaying1995@gmail.com>
2020-09-20 09:50:49 +0900

    コメント削除

--- src/main/java/com/example/making_restaurant/presentation/controller/api/MenuController.java
+++ src/main/java/com/example/making_restaurant/presentation/controller/api/MenuController.java
@@ -27,16 +27,6 @@ public class MenuController {
         return menuRepository.findAll();
     }
 
-    // 本当はここはPostMappingを使うべきなんだけど、Postするのめんどくさい
-    // そう。HTTP通信の規格で決められていて、取得がGET, 登録がPOST, 更新がPUT, 削除がDELETE
-    // になってる。
-    // 一応そういう役割だからそれに則ってやるのがベスト。
-    // 実際強制されてるわけじゃないから↓みたいに中身は登録とかやっても、一応は動く。
-    // @PostMapping // 普通はこっちを使う
-    // いま http://localhost:8080/api/menu/post (menuが抜けてた。。)ってやると、
-    // 「新メニュー」っていうメニューがどんどん登録されていく
-    // で、こういう感じにcontrollerで処理を書くのはfat controllerっていうダメな書き方
-    // コントローラは振り分け役。こういう処理はサービスの役割。
     @GetMapping("post")
     public Menu saveMenu() {
         Menu menu = new Menu();
